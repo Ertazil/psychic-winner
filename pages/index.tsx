145a2c21c5a4f6d8339867fade754aba13c62972
Ertazil <117348711+Ertazil@users.noreply.github.com>
2024-03-25 17:26:05 +0100

    test fonctionnement css

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,9 @@ export default function Homepage() {
         <div className="flex items-center justify-center w-full h-screen bg-neutral-900">
             <label>test</label>
             <input type="checkbox" name="test"/>
+            <textarea id="story" name="story" rows="5" cols="33">
+            It was a dark and stormy night...
+            </textarea>
         </div>
     );
 }
